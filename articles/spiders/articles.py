d276e7c5a74cc8874b61b2faddb30b838cdf7835
Alan Sutherland <sutherland_a@me.com>
2023-04-05 17:43:54 +0900

    Ssss

--- articles/spiders/articles.py
+++ articles/spiders/articles.py
@@ -12,9 +12,9 @@ class ArticlesSpider(scrapy.Spider):
     def parse(self, response):
         for article in response.xpath("//channel/item"):
             yield {
-                # 'title' : post.xpath('title//text()').extract_first(),
-                # 'link': post.xpath('link//text()').extract_first(),
-                scrapy.Request(response.urljoin(article.xpath('link//text()').extract_first()), callback=self.parse_article_page)
+                'title' : post.xpath('title//text()').extract_first(),
+                'link': post.xpath('link//text()').extract_first(),
+                # scrapy.Request(response.urljoin(article.xpath('link//text()').extract_first()), callback=self.parse_article_page)
                 # 'pubDate' : post.xpath('pubDate//text()').extract_first(),
             }
             # yield scrapy.Request(response.urljoin(article_url), callback=self.parse_article_page)
