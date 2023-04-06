27e9c950a4346d0f5ab71203a1c018a1ac1d19a3
Alan Sutherland <sutherland_a@me.com>
2023-04-06 16:53:07 +0900

    Fix pgql

--- articles/spiders/articles.py
+++ articles/spiders/articles.py
@@ -1,5 +1,6 @@
 import scrapy
 import requests
+from requests.exceptions import RequestException
 from articles.items import Article
 
 class ArticlesSpider(scrapy.Spider):
@@ -20,22 +21,23 @@ class ArticlesSpider(scrapy.Spider):
                 })
 
     def parse_article(self, response):
-        # ... scraping code ...
         scraped_title = response.meta["title"]
         scraped_link = response.url
         scraped_pubDate = response.meta["pubDate"]
         scraped_html = response.css(".post-content").get()
         scraped_text = "".join(response.css(".post-content *::text").getall())
 
-        # Send a POST request to the Flask API with the scraped data
-        api_url = "https://gentle-earth-02543.herokuapp.com/api/save_article"  # Update this URL if your Flask app is hosted elsewhere
-        requests.post(api_url, json={
-            "title": scraped_title,
-            "pubDate": scraped_pubDate,
-            "link": scraped_link,
-            "text": scraped_text,
-            "html": scraped_html
-        })
+        api_url = "https://gentle-earth-02543.herokuapp.com/api/save_article"
+        try:
+            requests.post(api_url, json={
+                "title": scraped_title,
+                "pubDate": scraped_pubDate,
+                "link": scraped_link,
+                "text": scraped_text,
+                "html": scraped_html
+            })
+        except RequestException as e:
+            self.logger.error(f"Error sending data to the API: {e}")
 
         yield {
             "title": scraped_title,
@@ -44,51 +46,3 @@ class ArticlesSpider(scrapy.Spider):
             "text": scraped_text,
             "html": scraped_html
         }
-
-
-# import scrapy
-# from articles.items import Article
-# from app import app, Article as ArticleModel, db
-
-# class ArticlesSpider(scrapy.Spider):
-#     name = "articles"
-#     allowed_domains = ["cointelegraph.com"]
-#     start_urls = [
-#         'https://cointelegraph.com/rss',
-#     ]
-
-#     def parse(self, response):
-#         items = response.xpath("//item")
-#         for item in items:
-#             link = item.xpath("link/text()").get()
-#             if link:
-#                 yield scrapy.Request(link, callback=self.parse_article, meta={
-#                     "title": item.xpath("title/text()").get(),
-#                     "pubDate": item.xpath("pubDate/text()").get(),
-#                 })
-
-#     def parse_article(self, response):
-#         scraped_title = response.meta["title"]
-#         scraped_link = response.url
-#         scraped_pubDate = response.meta["pubDate"]
-#         scraped_html = response.css(".post-content").get()
-#         scraped_text = "".join(response.css(".post-content *::text").getall())
-
-#         with app.app_context():
-#             article = ArticleModel(
-#                 title=scraped_title,
-#                 pubDate=scraped_pubDate,
-#                 link=scraped_link,
-#                 text=scraped_text,
-#                 html=scraped_html
-#             )
-#             db.session.add(article)
-#             db.session.commit()
-
-#         yield {
-#             "title": scraped_title,
-#             "pubDate": scraped_pubDate,
-#             "link": scraped_link,
-#             "text": scraped_text,
-#             "html": scraped_html
-#         }
