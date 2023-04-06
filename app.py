0c43743374449842a70e3c45bd61cfea0369b393
Alan Sutherland <sutherland_a@me.com>
2023-04-06 12:01:33 +0900

    View endpoint added

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///articles.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 api = Api(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    pubDate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    link = db.Column(db.String, nullable=False)
+    text = db.Column(db.Text, nullable=True)
+    html = db.Column(db.Text, nullable=True)
+    content_translated = db.Column(db.Text, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "pubDate": self.pubDate.isoformat(),
+            "link": self.link,
+            "text": self.text,
+            "html": self.html,
+            "content_translated": self.content_translated,
+        }
 
 class SaveArticleResource(Resource):
     def post(self):
         data = request.get_json()
-        article = ArticleModel(
+        article = Article(
             title=data["title"],
             pubDate=data["pubDate"],
             link=data["link"],
@@ -23,7 +43,13 @@ class SaveArticleResource(Resource):
         db.session.commit()
         return {"message": "Article saved successfully."}
 
+class GetAllArticlesResource(Resource):
+    def get(self):
+        articles = Article.query.all()
+        return jsonify([article.to_dict() for article in articles])
+
 api.add_resource(SaveArticleResource, "/api/save_article")
+api.add_resource(GetAllArticlesResource, "/api/get_all_articles")
 
 if __name__ == "__main__":
     app.run(debug=True)
