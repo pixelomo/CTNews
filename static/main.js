227658069a1e38803972f4ca09c985d102daa4d8
Alan Sutherland <sutherland_a@me.com>
2023-04-07 10:32:46 +0900

    TinyMCE

--- static/main.js
+++ static/main.js
@@ -24,3 +24,10 @@ $(document).ready(function () {
 
     loadArticles();
 });
+
+
+// tinymce.init({
+//     selector: 'textarea',
+//     plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+//     toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+//   });
