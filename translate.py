b52258d33d1840da5747e9e74f7c7ca1f12e18eb
Alan Sutherland <sutherland_a@me.com>
2023-04-07 12:05:58 +0900

    Limit

--- translate.py
+++ translate.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def translate_with_gpt4(text, model="your_model_name"):
+def translate_with_gpt4(text, model="gpt-3.5-turbo"):
     response = openai.Completion.create(
         engine=model,
         prompt=f"Translate the following English text to Japanese:\n{text}\nTranslation:",
-        max_tokens=100,  # Adjust the number of tokens based on the model limit
+        max_tokens=4000,  # Adjust the number of tokens based on the model limit
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0.3,
     )
 
     return response.choices[0].text.strip()
