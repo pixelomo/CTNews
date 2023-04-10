2025119cb9129036ce9e56da165cba18cd7b6d26
Alan Sutherland <sutherland_a@me.com>
2023-04-10 15:04:06 +0900

    Push d

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,17 +1,26 @@
 from app import app, db, Article
+from sqlalchemy.exc import IntegrityError
 
 def remove_duplicates():
     with app.app_context():
         articles = Article.query.all()
-        unique_articles = {}
+        unique_links = set()
+        duplicates = []
 
         for article in articles:
-            if article.link not in unique_articles:
-                unique_articles[article.link] = article
+            if article.link in unique_links:
+                duplicates.append(article)
             else:
-                db.session.delete(article)
+                unique_links.add(article.link)
 
-        db.session.commit()
+        for duplicate in duplicates:
+            try:
+                db.session.delete(duplicate)
+                db.session.commit()
+                print(f"Deleted duplicate article: {duplicate.title}")
+            except IntegrityError:
+                db.session.rollback()
+                print("Error deleting duplicate article.")
 
 if __name__ == "__main__":
     remove_duplicates()
