8f6cff3fc8fee6a4e27101e0626e29750aaf65f6
Alan Sutherland <sutherland_a@me.com>
2023-04-13 18:31:50 +0900

    Celery

--- celery_config.py
+++ celery_config.py
@@ -4,3 +4,4 @@ import os
 broker_url = os.environ.get('REDIS_URL')
 
 celery = Celery('translation_tasks', broker=broker_url)
+celery.conf.update(task_serializer='json', accept_content=['json'], result_serializer='json', timezone='UTC', enable_utc=True)
