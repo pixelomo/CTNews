e28330e52a5f23332fc5c0ad83beee5c79d51b97
Alan Sutherland <sutherland_a@me.com>
2023-04-13 11:25:48 +0900

    Spiders are go

--- articles/pipelines.py
+++ articles/pipelines.py
@@ -94,3 +94,10 @@ class ArticlesPipeline(object):
 
         return item
 
+if __name__ == "__main__":
+    from scrapy.crawler import CrawlerProcess
+    from scrapy.utils.project import get_project_settings
+
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('articles')
+    process.start()
