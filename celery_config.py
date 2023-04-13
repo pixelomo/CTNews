4657d02780b53cf10bf8a7aef97189edf21bcfb8
Alan Sutherland <sutherland_a@me.com>
2023-04-13 18:29:09 +0900

    Celery

--- celery_config.py
+++ celery_config.py
@@ -1,10 +1,6 @@
-from flask import Flask
 from celery import Celery
 import os
 
-app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = os.environ.get('REDIS_URL')
-app.config['CELERY_RESULT_BACKEND'] = os.environ.get('REDIS_URL')
+broker_url = os.environ.get('REDIS_URL')
 
-celery = Celery('translation_tasks', broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+celery = Celery('translation_tasks', broker=broker_url)
