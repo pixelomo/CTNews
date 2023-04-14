f6f804d82cdb50651fc808a0a8f9a6f849daaf03
Alan Sutherland <sutherland_a@me.com>
2023-04-14 09:23:04 +0900

    Celery

--- celery_config.py
+++ celery_config.py
@@ -1,15 +1,19 @@
 from celery import Celery
 import os
-import ssl  # Add this import
+import ssl
 
-broker_url = os.environ.get('REDIS')
+broker_url = os.environ.get('REDIS_URL')
 
-celery_app = Celery('translation_tasks', broker=broker_url)
+ssl_options = {
+    'ssl_cert_reqs': ssl.CERT_NONE,
+}
+
+celery_app = Celery('translation_tasks', broker=broker_url, broker_use_ssl=ssl_options)
 celery_app.conf.update(
     task_serializer='json',
     accept_content=['json'],
     result_serializer='json',
     timezone='UTC',
     enable_utc=True,
-    broker_use_ssl={'ssl_cert_reqs': ssl.CERT_NONE}  # Add this line
 )
+
