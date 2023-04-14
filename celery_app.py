cda0b9e607f2c652d27038dc621542e2b469e553
Alan Sutherland <sutherland_a@me.com>
2023-04-14 11:34:29 +0900

    Celery

--- celery_app.py
+++ celery_app.py
@@ -1,19 +1,18 @@
 from dotenv import load_dotenv
 from celery import Celery
 import os
-import redis
-from urllib.parse import urlparse
 
 load_dotenv()
 
-url = urlparse(os.environ.get("REDIS_URL"))
-r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=True, ssl_cert_reqs=None)
+REDIS_URL = os.environ.get("REDIS_URL")
 
-celery_app = Celery('translation_tasks', broker=r)
+celery_app = Celery('translation_tasks', broker=REDIS_URL, backend=REDIS_URL)
 celery_app.conf.update(
     task_serializer='json',
     accept_content=['json'],
     result_serializer='json',
     timezone='UTC',
     enable_utc=True,
+    broker_use_ssl=True,
+    result_backend_use_ssl=True,
 )
