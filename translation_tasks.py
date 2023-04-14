a54b82b1836200dbdb90b6067626a3816819ee38
Alan Sutherland <sutherland_a@me.com>
2023-04-14 09:48:00 +0900

    Celery

--- translation_tasks.py
+++ translation_tasks.py
@@ -1,7 +1,7 @@
 from celery_config import celery_app
 from translate import translate_with_gpt
 
-@celery.task
+@celery_app.task
 def perform_translation(text, target_language):
     translated_text = translate_with_gpt(text, target_language)
     return translated_text
