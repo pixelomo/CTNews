7511bc60c25e5df44bf9bdffa0a7740472063255
Alan Sutherland <sutherland_a@me.com>
2023-04-14 11:22:00 +0900

    Celery

--- celery_app.py
+++ celery_app.py
@@ -5,16 +5,11 @@ import redis
 from urllib.parse import urlparse
 
 load_dotenv()
-REDIS_URL = os.environ.get("REDIS_URL")
-url = urlparse(REDIS_URL)
 
-ssl_options = {
-    'ssl_cert_reqs': None,
-}
+url = urlparse(os.environ.get("REDIS_URL"))
+r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=True, ssl_cert_reqs=None)
 
-redis_url_with_ssl = f'redis://{url.username}:{url.password}@{url.hostname}:{url.port}/0'
-
-celery_app = Celery('translation_tasks', broker=redis_url_with_ssl, backend=REDIS_URL, broker_use_ssl=ssl_options)
+celery_app = Celery('translation_tasks', broker=r)
 celery_app.conf.update(
     task_serializer='json',
     accept_content=['json'],
