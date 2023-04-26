778b6189052829cc56a33c1ed822c51b8ba12710
Alan Sutherland <sutherland_a@me.com>
2023-04-26 16:17:02 +0900

    Shut up heroku

---
+++ migrations/versions/00e57c2afe7b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 00e57c2afe7b
+Revises: af14ca7678c5
+Create Date: 2023-04-26 16:14:05.890946
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '00e57c2afe7b'
+down_revision = 'af14ca7678c5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
