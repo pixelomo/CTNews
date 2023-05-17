91ce465064406a277e91df7519b8d8a1a9e87beb
Alan Sutherland <sutherland_a@me.com>
2023-05-17 15:51:56 +0900

    User agent

--- articles/settings.py
+++ articles/settings.py
@@ -6,6 +6,7 @@ NEWSPIDER_MODULE = 'articles.spiders'
 DOWNLOAD_TIMEOUT = 240  # Set the download timeout
 DOWNLOAD_DELAY = 2  # Add a delay between requests
 HTTPCACHE_ENABLED = True
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0'
 ITEM_PIPELINES = {
     'articles.pipelines.ArticlesPipeline': 300,
 }
