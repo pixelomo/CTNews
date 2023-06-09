fab20969df1743fb742fbaa5a8c9e83ae1151958
Alan Sutherland <sutherland_a@me.com>
2023-06-09 15:02:16 +0900

    Disable CN

--- articles/spiders/run_spider.py
+++ articles/spiders/run_spider.py
@@ -7,7 +7,7 @@ def main():
     subprocess.run(["scrapy", "crawl", "ambcrypto"])
     subprocess.run(["scrapy", "crawl", "blockworks"])
     subprocess.run(["scrapy", "crawl", "theblock"])
-    subprocess.run(["scrapy", "crawl", "cryptonews"])
+    # subprocess.run(["scrapy", "crawl", "cryptonews"])
     subprocess.run(["scrapy", "crawl", "coindesk"])
 
 if __name__ == "__main__":
