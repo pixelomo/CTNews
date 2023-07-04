cce4bed8a79d8d65eba3fff17345b52fcf3b0e21
Alan Sutherland <sutherland_a@me.com>
2023-07-04 11:47:53 +0900

    Db.session

--- articles/stats_pipelines.py
+++ articles/stats_pipelines.py
@@ -3,7 +3,6 @@ from app import app, db, ArticleStats
 class StatsPipeline(object):
     def process_item(self, item, spider):
         with app.app_context():
-            session = db.create_scoped_session()
             article = ArticleStats()
 
             article.pubDate = item["pubDate"]
@@ -11,13 +10,13 @@ class StatsPipeline(object):
             article.character_count = len(item["text"])
 
             try:
-                session.add(article)
-                session.commit()
+                db.session.add(article)
+                db.session.commit()
             except Exception as e:
-                session.rollback()
-                print(f"Failed to save article: {e}")
+                db.session.rollback()
+                print(f"Failed to save article stats: {e}")
                 raise
             finally:
-                session.close()
+                db.session.close()
 
             return item
